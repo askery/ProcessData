b01ec134256261661a2e255d5ef928bd7df7e55b
askery <askery@gmail.com>
2016-04-04 19:36:32 -0400

    Fix typos

--- ProcessData.py
+++ ProcessData.py
@@ -4,7 +4,7 @@ Created on Sun Oct 11 12:05:41 2015
 
 @author: Askery Canabarro
 
-THIS CODE IS TO COMPUTE MINUTE DATA FROM STOCK MARKET (BRAZILIAN STOCK LAYOUT ATTACHED)
+THIS CODE IS TO PREPROCESS DATA FROM STOCK MARKET (BRAZILIAN STOCK LAYOUT ATTACHED)
 """
 
 # THIS IS TO DETERMINE THE COMPUTATIONAL TIME DURATION
@@ -14,7 +14,7 @@ start=datetime.now()
 
 from pyspark import SparkContext
 #1 ALL DAILY FILES ARE IN THE SAME FOLDER - JUST ADDRESSING THE FOLDER PATH IS ENOUGHT TO GATHER EVERYTHING
-logFile = "/media/data/rowData_201304_201601/NEG_20130920.TXT"  # FILE FROM SYSTEM
+logFile = "/media/data/rowData_201304_201601/NEG_20151125.TXT"  # FILE FROM SYSTEM
 #
 sc = SparkContext("local", "Minute Data")
 
@@ -23,14 +23,14 @@ logData = sc.textFile(logFile)
 
 
 #2 Selection of stocks - here we are slecting the 61 IBOV (main BM&FBovespa Index) stocks 
-stocks = ['ABEV3','BBAS3']
+#stocks = ['ABEV3','BBAS3']
 
-#stocks = ['ABEV3','BBAS3','BBDC3','BBDC4','BBSE3','BRAP4','BRFS3','BRKM5','BRML3','BVMF3',
-#'CCRO3','CESP6','CIEL3','CMIG4','CPFE3','CPLE6','CSAN3','CSNA3','CTIP3','CYRE3',
-#'ECOR3','EMBR3','ENBR3','EQTL3','ESTC3','FIBR3','GGBR4','GOAU4','HGTX3','HYPE3',
-#'ITSA4','ITUB4','JBSS3','KLBN11','KROT3','LAME4','LREN3','MRFG3','MRVE3','MULT3',
-#'NATU3','OIBR3','PCAR4','PETR3','PETR4','QUAL3','RADL3','RENT3','RUMO3','SANB11',
-#'SBSP3','SMLE3','SUZB5','TBLE3','TIMP3','UGPA3','USIM5','VALE3','VALE5','VIVT4','WEGE3']
+stocks = ['ABEV3','BBAS3','BBDC3','BBDC4','BBSE3','BRAP4','BRFS3','BRKM5','BRML3','BVMF3',
+'CCRO3','CESP6','CIEL3','CMIG4','CPFE3','CPLE6','CSAN3','CSNA3','CTIP3','CYRE3',
+'ECOR3','EMBR3','ENBR3','EQTL3','ESTC3','FIBR3','GGBR4','GOAU4','HGTX3','HYPE3',
+'ITSA4','ITUB4','JBSS3','KLBN11','KROT3','LAME4','LREN3','MRFG3','MRVE3','MULT3',
+'NATU3','OIBR3','PCAR4','PETR3','PETR4','QUAL3','RADL3','RENT3','RUMO3','SANB11',
+'SBSP3','SMLE3','SUZB5','TBLE3','TIMP3','UGPA3','USIM5','VALE3','VALE5','VIVT4','WEGE3']
 
 #3 selection filter
 def filterStock(line):
@@ -45,34 +45,31 @@ def makeColumns(line):
     time    = pieces[5].split(':')      # Extract hours, minutes, seconds and miliseconds from trade time (HH:MM:SS.NNN)
     hour    = time[0]                   # extract hour - fisrt element in 'time'   
     minu    = time[1]                   # extract minutes - second element in 'time'
-    sec     = time[2].split('.')
-    key     = symb + date + hour + minu # KEY DEFINITION AS CONCATENATION OF TIME COLUMNS
-    output  = str(hour) + str(minu) + "   " + symb + "   " + str("%.2f" % float(pieces[3])) + "   " + str(int(pieces[4])) # OUTOUT FORMAT WITHOUT 'U', ',' OR '()'
+    sec     = time[2].split('.')        # separate seconds from miliseconds
+    key     = symb + hour + minu + sec[0] + sec[1]  # key definition as concatenation of columns
+    # OUTOUT FORMAT WITHOUT 'U', ',' OR '()'
+    output  = date + "   " + str(hour)+":"+str(minu)+":"+str(sec[0])+"."+str(sec[1]) + "   " +  str("%.2f" % float(pieces[3])) + "   " + str(int(pieces[4])) 
     return (key, output)
 
-#5 SELECT FIRST MINUTE OCCURENCY. TO PICK LAST USE 'return b;'
-def minData(a, b):
-    return a;
-
-#6 THIS IS TO DEAL WITH DIFFERENT STOCK IN DISTINCT OUTPUT FILES
+#5 function to separate distinct stocks in different output files
 def separate(a):
     if stock in a[0]:
         return (a[0],a[1])    
 
-#7 THIS IS THE APPLICATION OF SOME TRANSFORMATIONS AND ACTIONS NEEDED ON THE 'logData' RDD
+#6 THIS IS THE APPLICATION OF SOME TRANSFORMATIONS AND ACTIONS NEEDED ON THE 'logData' RDD
 #   FIRST A FILTER TO DEAL ONLY WITH THE DESIRED STOCKS - filter(filterStock)
 #   THEN WE SELECT THE COLUMNS OF INTEREST IN 'map(makeColumns)'
 #   NOW WE EXTRACT THE MINUTE DATA USING 'reduceByKey(minData)'
 #   FINALLY WE SORT BY KEY
-result= logData.filter(filterStock).map(makeColumns).reduceByKey(minData).sortByKey()
+result  = logData.filter(filterStock).map(makeColumns).sortByKey()
 
 
-#8 OUTPUT
+#7 OUTPUT
 #   FIRST WE FILTER IF stock IS IN THE KEYS - filter(separate)   
 #   ".values()" IS TO TAKE ONLY THE output OF THE (KEY, VALUE) PAIRS
 #   ".coalesce(1)" PUT EVERYTHING IN JUST ONE OUTPUT FILE, NAMED AS THE ELEMENTS IN THE stocks LIST
 for stock in stocks:
     result.filter(separate).values().coalesce(1).saveAsTextFile(str(stock))
 
-# PRINT THE DURATION OF THE EXECUTION OF THE PROGRAM
+# print the duration of execution
 print datetime.now() - start
